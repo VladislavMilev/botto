47367e62002cb6c37f67a27180400bb8ef4f63d6
jimroot <jimroot@N4440.TM>
2019-01-08 16:29:11 +0200

    json vs git

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,128 +1,133 @@
-import com.vdurmont.emoji.EmojiParser;
+import com.google.gson.Gson;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Random;
+import java.util.List;
 
+//https://graph.facebook.com/me
 
 public class Bot extends TelegramLongPollingBot {
 
-                @Override
-                public String getBotUsername() {
-                        return "jimroot_bot:";
-                }
+    @Override
+    public String getBotUsername() {
+        return "gluten_bot";
+    }
 
-                @Override
-                public String getBotToken() {
-                        return "740174724:AAG_93TnzZPKG79qc010Lvyko5FDC3khQBY";
-                }
+    @Override
+    public String getBotToken() {
+        return "712052677:AAE5kn6gZWRbbUDMSTTXSFd1Fw1djBdAKso";
+    }
 
-                @Override
-                public void onUpdateReceived(Update update) {
+    private static String readUrl(String urlString) throws Exception {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
 
-                        if (update.hasMessage() && update.getMessage().hasText()) {
-
-                                String user_first_name = update.getMessage().getChat().getFirstName();
-                                String user_last_name = update.getMessage().getChat().getLastName();
-                                long user_id = update.getMessage().getChat().getId();
-                                String message_text = update.getMessage().getText();
-                                long chat_id = update.getMessage().getChatId();
-                                String answer = message_text;
-
-                           SendMessage message = new SendMessage()
-                                        .setChatId(chat_id)
-                                        .setText(answer);
-                                log(user_first_name, user_last_name, Long.toString(user_id), message_text, answer);
-                                try {
-                                        execute(message);
-                                } catch (TelegramApiException e) {
-                                        e.printStackTrace();
-                                }
-                        }
-
-                        Message message = update.getMessage();
-
-                        String km = "/ktomudak";
-
-                        String kmM[] = {"Ты", "Simple", "Твой друг", "Твой начальник", "Floki", "Создатель TM"};
-
-                        int idxkM = new Random().nextInt(kmM.length);
-                        String kmMM = (kmM[idxkM]);
-
-                        String rsk = "/randSuperKoder";
-
-                        String supercoder[] = {"Reiko", "Panda", "Starboy", "Inright", "Точно не Simple", "Vortex", "MJHead"};
-
-                        int idx = new Random().nextInt(supercoder.length);
-                        String random = (supercoder[idx]);
-
-                        String lt = "/theBestTester";
-                        String cbz = "/choBudetZavtra";
-
-                        String cbzM[] = {"Тебя уволят", "Опоздаешь на работу", "Забудешь пропуск", "Скажут гадости", "Не будет писаться код", "Сэндер даст задание", "Дизбонус епта"};
-
-                        int idxCBZ = new Random().nextInt(cbzM.length);
-                        String randomCBZ = (cbzM[idxCBZ]);
-
-                        String cpzp = "/kogdaPodnimutZP";
-
-                        if (message != null && message.hasText()) {
-
-                           if (message.getText().equals("/start")){
-                                        sendMsg(message,
-                                                "Кто мудак:" + " - " + km + "\n" +
-                                                "Рандомный суперкодер:" + " - " + rsk + "\n" +
-                                                "Кто лучший тостер:" + " - " + lt + "\n" +
-                                                "Что будет завтра:" + " - " + cbz + "\n" +
-                                                "Когда поднимут ЗП:" + " - " + cpzp + "\n"
-                                        );}
-                           else if (message.getText().equals(km)){
-                              sendMsg(message, kmMM + EmojiParser.parseToUnicode("\n:smile:"));
-                           }
-                           else if (message.getText().equals(rsk)){
-                              sendMsg(message, random + EmojiParser.parseToUnicode("\n:smile:"));
-                           }
-                           else if (message.getText().equals(lt)){
-                              sendMsg(message, "Floki is a SUPERTOSTER." + EmojiParser.parseToUnicode("\n:smile:"));
-                           }
-                           else if (message.getText().equals(cbz)){
-                              sendMsg(message, randomCBZ + EmojiParser.parseToUnicode("\n:smile:"));
-                           }
-                           else if (message.getText().equals(cpzp)){
-                              sendMsg(message, "Тебе - никогда!" + EmojiParser.parseToUnicode("\n:smile:"));
-                           }
-                           else{
-                              sendMsg(message, "What?");
-                           }
-                        }
-                }
-
-                private void sendMsg(Message message, String text) {
-                        SendMessage sendMessage = new SendMessage();
-                        sendMessage.enableMarkdown(true);
-                        sendMessage.setChatId(message.getChatId().toString());
-                        sendMessage.setReplyToMessageId(message.getMessageId());
-                        sendMessage.setText(text);
-                        try {
-                                sendMessage(sendMessage);
-                        } catch (TelegramApiException e) {
-                                e.printStackTrace();
-                        }
-                }
-
-                private void log(String first_name, String last_name, String user_id, String txt, String bot_answer) {
-                        System.out.println("\n ----------------------------");
-                        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-                        Date date = new Date();
-                        System.out.println(dateFormat.format(date));
-                        System.out.println("Message from " + first_name + " " + last_name + ". (id = " + user_id + ") \n Text - " + txt);
-                        System.out.println("Bot answer: \n Text - " + bot_answer);
-                }
-
+            return buffer.toString();
+        } finally {
+            if (reader != null)
+                reader.close();
         }
+    }
+
+    static class Item {
+        String title;
+        String link;
+        String description;
+        String img;
+    }
+
+    static class Page {
+        String title;
+        String link;
+        String description;
+        String language;
+        List<Item> items;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        String json = null;
+        try {
+            json = readUrl("https://raw.githubusercontent.com/VladislavMilev/json_telegram_bot/master/telegram.json");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Gson gson = new Gson();
+        Page page = gson.fromJson(json, Page.class);
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+
+            String user_first_name = update.getMessage().getChat().getFirstName();
+            String user_last_name = update.getMessage().getChat().getLastName();
+            long user_id = update.getMessage().getChat().getId();
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            String answer = message_text;
+
+            SendMessage message = new SendMessage()
+                    .setChatId(chat_id)
+                    .setText(answer);
+            log(user_first_name, user_last_name, Long.toString(user_id), message_text, answer);
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Message message = update.getMessage();
+
+        if (message != null && message.hasText()) {
+
+            if (message.getText().equals("/start")) {
+                for (Item item : page.items) {
+                    sendMsg(message, item.img, item.link);
+                }
+
+            } else {
+                sendMsg(message, "What?", "WHAT?");
+            }
+        }
+    }
+
+    private void sendMsg(Message message, String text, String link) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void log(String first_name, String last_name, String user_id, String txt, String bot_answer) {
+        System.out.println("\n ----------------------------");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+        System.out.println("Message from " + first_name + " " + last_name + ". (id = " + user_id + ") \n Text - " + txt);
+        System.out.println("Bot answer: \n Text - " + bot_answer);
+    }
+
+}
